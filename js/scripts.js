5c5c98cec81626ba440e0b1c2bba43806a97cd4e
SarahWK <sarahkulubya@gmail.com>
2018-06-20 10:47:41 +0300

    I changed the layout of my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
+//Calculator Project
 var add = function(number1, number2) {
        return number1 + number2;
      };
 
-     var number1 = parseInt(prompt("Enter a number:"));
-     var number2 = parseInt(prompt("Enter another number:"));
-     var result = add(number1, number2);
-     alert(result)
 
-     var multiply = function (number3, number4) {
-       return number3 * number4;
+     var multiply = function (number1, number2) {
+       return number1 * number2;
      };
 
-     var number3 = parseInt(prompt("Enter a number:"));
-     var number4 = parseInt(prompt("Enter another number:"));
-     var result = multiply(number3, number4);
-     alert(result)
-
-     var divide = function(number5, number6) {
-        return number5 / number6;
+     var divide = function(number1, number2) {
+        return number1 / number2;
      };
 
-     var number5 = parseInt(prompt ("Enter a number:"));
-     var number6 = parseInt(prompt("Enter another number:"));
-     var result = divide(number5, number6;
-     alert(result)
+     var subtract = function(number1, number2) {
+       return number1 - number2;
+     }
+
+//user responds 1
+var number1 = parseInt(prompt("Enter a number:"));
+alert(number1);
+
+//user responds 2
+var number2 = parseInt(prompt("Enter another number:"));
+alert(number2);
+
+//answer
+    alert(add(number1,number2));
+    alert(multiply(number1, number2));
+    alert(divide(number1, number2));
+    alert(subtract(number1, number2));
