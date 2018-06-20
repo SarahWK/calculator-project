98233009fd98042a6572d23e3021b1fac59db0c9
SarahWK <sarahkulubya@gmail.com>
2018-06-20 10:30:22 +0300

    I added some new functions to add,multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ var add = function(number1, number2) {
      var number2 = parseInt(prompt("Enter another number:"));
      var result = add(number1, number2);
      alert(result)
+
+     var multiply = function (number3, number4) {
+       return number3 * number4;
+     };
+
+     var number3 = parseInt(prompt("Enter a number:"));
+     var number4 = parseInt(prompt("Enter another number:"));
+     var result = multiply(number3, number4);
+     alert(result)
+
+     var divide = function(number5, number6) {
+        return number5 / number6;
+     };
+
+     var number5 = parseInt(prompt ("Enter a number:"));
+     var number6 = parseInt(prompt("Enter another number:"));
+     var result = divide(number5, number6;
+     alert(result)
